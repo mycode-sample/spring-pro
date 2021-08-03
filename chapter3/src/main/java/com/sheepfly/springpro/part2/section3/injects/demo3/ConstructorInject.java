5feac82052978db590c33d20f60fa4ec675d1554
sheepfly <developer@noreply.email.com>
2021-08-04 00:15:14 +0800

    构造函数注入

---
+++ chapter3/src/main/java/com/sheepfly/springpro/part2/section3/injects/demo3/ConstructorInject.java
@@ -0,0 +1,14 @@
+package com.sheepfly.springpro.part2.section3.injects.demo3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConstructorInject {
+    public static void main(String[] args) {
+        String path = "com/sheepfly/springpro/part2/section3/ConstructorInject.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(path);
+        UnknownConstructor unknownConstructor = ctx.getBean("unknownConstructor2", UnknownConstructor.class);
+        unknownConstructor.f();
+        System.out.println("ctx:" + ctx);
+    }
+}
